a13977769da93253aca09f6e0177aca635e252ed
Neri Filho <nerifilho1256@gmail.com>
2023-12-30 13:26:27 -0300

    URL

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import manipular_dados, export_csv, remover_dado
 
 urlpatterns = [
-    path('manipular_dados/', manipular_dados, name='manipular_dados'),
+    path('', manipular_dados, name='manipular_dados'),
     path('export_csv/', export_csv, name='export_csv'),
     path('remover_dado/<int:dado_id>/', remover_dado, name='remover_dado'),
 ]
--- testeDjango/urls.py
+++ testeDjango/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('meu_app/', include('meu_app.urls')),
+    path('', include('meu_app.urls')),
 ]
